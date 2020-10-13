371dfc6287e12ec961d94a5c298fbc20ec7f4046
george.zhu <george.zhu@ibaiqiu.com>
2020-10-13 20:20:45 +0800

    changed

--- service-feign/src/main/java/com/zcy/service/SchedualServiceHi.java
+++ service-feign/src/main/java/com/zcy/service/SchedualServiceHi.java
@@ -9,9 +9,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  * Created by zcy on 2018/7/26.
  */
-@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)
+//@FeignClient(value = "service-hi",url = "${url}",fallback = SchedualServiceHiHystric.class)//调试开启url参数指定生产者机器
+@FeignClient(value = "service-hi", fallback = SchedualServiceHiHystric.class)
 @Service
 public interface SchedualServiceHi {
-    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
 }
