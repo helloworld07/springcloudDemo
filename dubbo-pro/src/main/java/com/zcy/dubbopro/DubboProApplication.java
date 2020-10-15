2f6461382d004442caf0e5881c660ce6ddfa74df
george.zhu <george.zhu@ibaiqiu.com>
2020-10-15 14:40:39 +0800

    changed

---
+++ dubbo-pro/src/main/java/com/zcy/dubbopro/DubboProApplication.java
@@ -0,0 +1,13 @@
+package com.zcy.dubbopro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubboProApplication.class, args);
+	}
+
+}
