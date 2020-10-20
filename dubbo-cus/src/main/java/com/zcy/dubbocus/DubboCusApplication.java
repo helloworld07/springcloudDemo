d143babccd558894da88b6a6d4bdcbed481286e7
george.zhu <george.zhu@ibaiqiu.com>
2020-10-20 16:50:47 +0800

    changed

---
+++ dubbo-cus/src/main/java/com/zcy/dubbocus/DubboCusApplication.java
@@ -0,0 +1,13 @@
+package com.zcy.dubbocus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboCusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubboCusApplication.class, args);
+	}
+
+}
